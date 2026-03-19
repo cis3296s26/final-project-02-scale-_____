0acdeca5478e26948909dba21cdd86b624cff351
BennyQian1 <tuq28313@temple.edu>
2026-03-19 18:33:19 -0400

    Added test player

---
+++ gameproject-1/Player/Player_Script/test_player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+
+@onready var animatedSprite = $AnimatedSprite2D
+
+# runs at launch
+func _physics_process(delta: float) -> void:
+	basic_movement(delta)
+	handle_animations()
+	handle_direction()
+	move_and_slide()
+
+func handle_animations() -> void:
+	if is_on_floor():
+		if velocity:
+			animatedSprite.play("run")
+		else:
+			animatedSprite.play("idle")
+	else:
+		if velocity.y < 0:
+			animatedSprite.play("jump")
+		else:
+			animatedSprite.play("fall")
+
+func handle_direction() -> void:
+	if velocity.x < 0:
+		animatedSprite.flip_h = true
+	elif velocity.x > 0:
+		animatedSprite.flip_h = false
+
+func basic_movement(delta: float) -> void:
+		# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
