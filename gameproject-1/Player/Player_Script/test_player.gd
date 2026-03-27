ba7a2db62cc03f4e3e71bdfe13757f74e1d86f00
BennyQian1 <tuq28313@temple.edu>
2026-03-26 21:19:42 -0400

    added implementation for dashing and gliding

--- gameproject-1/Player/Player_Script/test_player.gd
+++ gameproject-1/Player/Player_Script/test_player.gd
@@ -1,8 +1,20 @@
 extends CharacterBody2D
 
 
-@export var speed = 275.0
-@export var jump_velocity = -400.0
+@export var speed = 200.0
+var dash_velocity = 1.0
+var can_dash = true
+
+@export var gravity_cap = 980
+@export var jump_gravity = -480
+@export var glide_gravity = 80
+@export var threshold = 1
+
+var current_glide_gravity = gravity_cap
+var glide_timer = 0.0
+var on_ground = false
+
+var tween: Tween
 
 @onready var animatedSprite = $AnimatedSprite2D
 
@@ -33,28 +45,59 @@ func handle_direction() -> void:
 		animatedSprite.flip_h = false
 
 func basic_movement(delta: float) -> void:
-		# Add the gravity.
-	if not is_on_floor():
-		var gravity = get_gravity()
-		if velocity.y > 0:
-			# If falling, slower fall to glide
-			gravity *= 0.30
-		velocity += gravity * delta
+	if is_on_floor():
+		glide_timer = 0.0
+		current_glide_gravity = glide_gravity
+		on_ground = true
+		
+		if Input.is_action_just_pressed("jump") and on_ground:
+			velocity.y = jump_gravity
+			on_ground = false
+		
+	else:
+		if Input.is_action_pressed("jump") and velocity.y > 0:
+			glide_timer += delta*1.7
+			
+			if glide_timer > threshold:
+				current_glide_gravity *= 7
+				current_glide_gravity = min(current_glide_gravity, gravity_cap)
+			else:
+				current_glide_gravity = glide_gravity
+		else:
+			# Letting go early uses normal gravity
+			current_glide_gravity = gravity_cap
+		velocity.y += current_glide_gravity * delta
+	
+	if Input.is_action_just_pressed("shift") and can_dash:
+		dash_start()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction * (speed * dash_velocity)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+		
+func dash_start():
+	if not can_dash: 
+		return
+	
+	can_dash = false
+	
+	
+	
+	dash_velocity = 2
+	$DashDurationTimer.start(0.5) 
+	$DashCooldownTimer.start(0.6)
 
+func _on_dash_duration_timer_timeout() -> void:
+	dash_velocity = 1
+
+
+func _on_dash_cooldown_timer_timeout() -> void:
+	can_dash = true
+
+# more in-dept player functions
 func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_ESCAPE:
-			#Go to pause menu if Esc key pressed
-			get_tree().change_scene_to_file("res://pause_menu.tscn");
+	if event.is_action_pressed("ui_cancel"):
+		#Go to pause menu if Esc key pressed
+		get_tree().change_scene_to_file("res://pause_menu.tscn")
