1204ddc92c19cbd2d146d0838de797ad0614c16b
Michael Vital <tur21338@temple.edu>
2026-04-02 22:37:53 -0400

    Created Tutorial Scene
    New game from the main menu prompts if you want to play or skip the tutorial

---
+++ gameproject-1/scripts/tutorial_prompt.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+func _on_new_game_pressed() -> void:
+	print("Skipping Tutorial")
+	get_tree().change_scene_to_file("res://scenes/game.tscn");
+
+
+func _on_play_tutorial_pressed() -> void:
+	print("Start Tutorial")
+	get_tree().change_scene_to_file("res://scenes/game.tscn");
