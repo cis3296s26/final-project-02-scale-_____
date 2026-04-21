69867bff545cdc1d091ea370509151d6e551f847
JayMcK103 <tur78536@temple.edu>
2026-04-20 20:16:30 -0400

    Boss nerf and teleports to 2-2 and 2-3

--- gameproject-1/Enemy/EnemyScripts/laptopBossScripts/vs_projectile.gd
+++ gameproject-1/Enemy/EnemyScripts/laptopBossScripts/vs_projectile.gd
@@ -6,7 +6,7 @@ extends Area2D
 var target: Node2D = null
 var velocity: Vector2 = Vector2.ZERO
 
-@export var speed: float = 100.0
+@export var speed: float = 75.0
 @export var lifetime: float = 3.0
 @export var damage_value: int = 1
 
