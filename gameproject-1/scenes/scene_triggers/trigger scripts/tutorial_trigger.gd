dceddd0f8434529bd918d92b459a3541640a54d0
BennyQian1 <tuq28313@temple.edu>
2026-04-23 23:14:59 -0400

    testing

--- gameproject-1/scenes/scene_triggers/trigger scripts/tutorial_trigger.gd
+++ gameproject-1/scenes/scene_triggers/trigger scripts/tutorial_trigger.gd
@@ -1,19 +1,9 @@
 extends Area2D
 
-# This allows you to type the instruction directly in the Godot Inspector!
 @export_multiline var zone_message: String = "Insert Instruction Here"
 
 func _on_body_entered(body: Node2D) -> void:
-	# Check if the node entering is actually our Owl Player
 	if body.name == "Player": 
-		# Find the HUD in the current level
 		var hud = get_tree().current_scene.find_child("TutorialHUD")
 		if hud:
 			hud.update_text(zone_message)
-
-#func _on_body_exited(body: Node2D) -> void:
-	#if body.name == "Player":
-		#var hud = get_tree().current_scene.find_child("TutorialHUD")
-		#if hud:
-			## Optional: Reset the text or clear it when they leave the area
-			#hud.update_text("Next Stop: Cecil B. Moore")
